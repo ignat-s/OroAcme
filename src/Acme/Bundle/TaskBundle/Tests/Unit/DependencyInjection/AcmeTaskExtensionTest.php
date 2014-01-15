83d74718a05f17e7ed65893bce044c8b4389cc5d
Sergey Zhuravel <zhuravel.sergey@gmail.com>
2014-01-15 17:14:03 +0200

    BAP-2850: Add functional tests for controllers - add configuration and extension tests

--- src/Acme/Bundle/TaskBundle/Tests/Unit/DependencyInjection/AcmeTaskExtensionTest.php
+++ src/Acme/Bundle/TaskBundle/Tests/Unit/DependencyInjection/AcmeTaskExtensionTest.php
@@ -2,9 +2,10 @@
 
 namespace Acme\Bundle\TaskBundle\Tests\Unit\DependencyInjection;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-
 use Acme\Bundle\TaskBundle\DependencyInjection\AcmeTaskExtension;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Reference;
 
 class AcmeTaskExtensionTest extends \PHPUnit_Framework_TestCase
 {
@@ -24,9 +25,69 @@ class AcmeTaskExtensionTest extends \PHPUnit_Framework_TestCase
         $this->extension = new AcmeTaskExtension();
     }
 
-    public function testLoad()
+    /**
+     * @dataProvider loadParameterDataProvider
+     */
+    public function testLoadParameters($parameter)
     {
         $this->extension->load(array(), $this->container);
-        $this->assertTrue($this->container->hasParameter('acme_task.entity.class'));
+        $this->assertTrue($this->container->hasParameter($parameter));
+    }
+
+    public function loadParameterDataProvider()
+    {
+        return array(
+            'acme_task.entity.class' => array('acme_task.entity.class'),
+            'acme_task.form.type.task.class' => array('acme_task.form.type.task.class'),
+            'acme_task.form.type.task_api.class' => array('acme_task.form.type.task_api.class'),
+            'acme_task.statistics.class' => array('acme_task.statistics.class'),
+        );
+    }
+
+    /**
+     * @dataProvider loadServiceDataProvider
+     */
+    public function testLoadServices($service, $class, array $arguments, array $tags)
+    {
+        $this->extension->load(array(), $this->container);
+        $definition = $this->container->getDefinition($service);
+
+        $this->assertEquals($class, $definition->getClass());
+        $this->assertTrue($this->container->hasParameter(trim($class, '%')));
+
+        $this->assertEquals($arguments, $definition->getArguments());
+        $this->assertEquals($tags, $definition->getTags());
+    }
+
+    public function loadServiceDataProvider()
+    {
+        return array(
+            'acme_task.form.type.task' => array(
+                'service' => 'acme_task.form.type.task',
+                'class' => '%acme_task.form.type.task.class%',
+                'arguments' => array('%acme_task.entity.class%'),
+                'tags' => array(
+                    'form.type' => array(
+                        array('alias' => 'acme_task')
+                    )
+                )
+            ),
+            'acme_task.form.type.task_api' => array(
+                'service' => 'acme_task.form.type.task_api',
+                'class' => '%acme_task.form.type.task_api.class%',
+                'arguments' => array(),
+                'tags' => array(
+                    'form.type' => array(
+                        array('alias' => 'acme_task_api')
+                    )
+                )
+            ),
+            'acme_task.statistics' => array(
+                'service' => 'acme_task.statistics',
+                'class' => '%acme_task.statistics.class%',
+                'arguments' => array(new Reference('doctrine.orm.entity_manager')),
+                'tags' => array()
+            )
+        );
     }
 }
